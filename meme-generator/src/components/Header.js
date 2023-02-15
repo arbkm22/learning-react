b40592548f6f6a744602dcd07981cd267cd2661b
Bhaskar Mahto <arbkm22@gmail.com>
2023-02-15 09:06:23 +0530

    meme generator initial commit

---
+++ meme-generator/src/components/Header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import trollFace from '../assets/TrollFace.png';
+
+function Header() {
+    return (
+        <div className='header'>
+            <img src={trollFace} className='header-trollface' alt='trollface'></img>
+            <span className='header-title'>Meme Generator</span>
+            <span className='header-title-end'>React Course - Part 3</span>
+        </div>
+    );
+}
+
+export default Header;
