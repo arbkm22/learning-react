43325f493bb9ec80e876f0f5733d4338317a7de7
Bhaskar Mahto <arbkm22@gmail.com>
2023-08-29 00:48:32 +0530

    controlled components

--- jokes-app/src/App.js
+++ jokes-app/src/App.js
@@ -10,10 +10,78 @@ function App() {
 
     const [messages, setMessages] = useState(['a', 'b']);
 
+    const [formData, setFormData] = useState({
+        firstName: '',
+        lastName: '',
+        email: '',
+        comments: '',
+        isFriendly: true
+    });
+
+    console.log('isFriendly: ', formData.isFriendly);
+
+    console.log('formdata: ', formData);
+
+    function handleChange(event) {
+        const {name, value, type, checked} = event.target;
+        setFormData(prevData => {
+            return {
+                ...prevData,
+                [name]: type === 'checkbox' ? checked : value
+            }
+        });
+    }
+
+    function handleSubmit(event) {
+        event.preventDefaults();
+        console.log('formData: ', formData);
+    }
+
   return (
     <div>
         {jokeElements}
         {messages.length > 0 ? `You have ${messages.length} unread ${messages.length == 1 ? 'message' : 'messages'}` : `You're all caught up`}
+        <form onSubmit={handleSubmit}>
+            <input 
+                type="text"
+                placeholder="First Name"
+                onChange={handleChange}   
+                name='firstName'
+                value={formData.firstName}
+            />
+            <input 
+                type="text"
+                placeholder='Last Name'
+                onChange={handleChange}
+                name='lastName'
+                value={formData.lastName}
+            />
+            <input 
+                type="email"
+                placeholder='Email'
+                onChange={handleChange}
+                name='email'
+                value={formData.email}
+            />
+            <textarea 
+                placeholder='Comments?'
+                name='comments'
+                value={formData.comments}
+                onChange={handleChange}
+            />
+            <input 
+                type='checkbox'
+                checked={formData.isFriendly}
+                onChange={handleChange}
+                name='isFriendly'
+                id='isFriendly'
+            />
+            <label htmlFor='isFriendly'>Are you friendly?</label> 
+            <br />
+            <br />
+
+            <button>Submit</button>
+        </form>
     </div>
   );
 }
