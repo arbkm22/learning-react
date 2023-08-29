dea2a2e42f9955c766587f2f5cffa1af043cfa14
Bhaskar Mahto <arbkm22@gmail.com>
2023-08-30 01:18:59 +0530

    sign up form

--- sign-up/src/App.js
+++ sign-up/src/App.js
@@ -1,23 +1,82 @@
-import logo from './logo.svg';
+import React, { useState } from 'react'
 import './App.css';
 
 function App() {
+
+    const [formData, setFormData] = React.useState({
+        email: '',
+        password: '',
+        cnfpass: '',
+        newsletter: false
+    })
+
+    function handleChange(event) {
+        const {type, name, value, checked} = event.target;
+        setFormData(prevData => {
+            return {
+                ...prevData,
+                [name]: type === 'checkbox' ? checked : value
+            }
+        });
+    }
+
+    function handleSubmit(event) {
+        event.preventDefault();
+        console.log('formdata: ', formData);
+        if (formData.password === formData.cnfpass)
+            console.log('successfully signed up');
+        if (formData.newsletter)
+            console.log('subscribed');
+    }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <form onSubmit={handleSubmit}>
+            <div className='BoxCard'>
+                <div className='FormInput'>
+                    <input 
+                        type='email' 
+                        name='email'
+                        placeholder='email'
+                        onChange={handleChange}
+                        value={formData.email}
+                    />
+                </div>
+                <div className='FormInput'>
+                    <input
+                        type='password'
+                        name='password'
+                        placeholder='password'
+                        onChange={handleChange}
+                        value={formData.password}
+                    />
+                </div>
+                <div className='FormInput'>
+                    <input
+                        type='password'
+                        name='cnfpass'
+                        placeholder='confirm password'
+                        onChange={handleChange}
+                        value={formData.cnfpass}
+                    />
+                </div>
+                <div className='FormInput'>
+                    <div className='CheckBox'>
+                        <input
+                            type='checkbox'
+                            name='newsletter'
+                            checked={formData.newsletter}
+                            onChange={handleChange}
+                            id='newsletter'
+                        />
+                        <label htmlFor='newsletter'>I want to join the newsletter</label>
+                    </div>
+                </div>
+                <div className='submitButton'>
+                    <button>Sign Up</button>
+                </div>
+            </div>
+        </form>
     </div>
   );
 }
