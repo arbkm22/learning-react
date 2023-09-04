e7fffa8d43ffff7032aaaef93ca795591a28dc28
Bhaskar Mahto <arbkm22@gmail.com>
2023-09-04 23:53:08 +0530

    warm up challenge - add dark mode functionality

--- project1/src/App.js
+++ project1/src/App.js
@@ -1,13 +1,16 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import Main from './components/Main.js';
 import Navbar from './components/Navbar.js';
 
 function App() {
+
+    const [darkMode, setDarkMode] = useState(false);
+
     return (
         <div className="container">
-            <Navbar />
-            <Main />            
+            <Navbar mode={darkMode} />
+            <Main mode={darkMode} />            
         </div>
     )
 }
--- project1/src/components/Navbar.js
+++ project1/src/components/Navbar.js
@@ -1,13 +1,23 @@
 import logo from './logo192.png';
 
-function Navbar() {
+function Navbar(props) {
+    console.log('props: ', props.mode);
     return (
         <div className="navbar">
             <div className="navbar-left">
                 <img src={logo} alt="React Logo"/>
                 <h1>ReactFacts</h1>
             </div>
-            <h2>React Course - Project 1</h2>
+            <div className="toggler">
+                <p className="toggler--light">Light</p>
+                <div 
+                    className="toggler--slider" 
+                    onClick={props.toggleDarkMode}
+                >
+                    <div className="toggler--slider--circle"></div>
+                </div>
+                <p className="toggler--dark">Dark</p>
+            </div>
         </div>
     )
 }
